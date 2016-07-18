7c7d50ad9b949ca75916d3aba5b6dae3d0259ab0
wei bai <baiwei0427@sina.com>
2016-07-18 19:53:24 +0800

    Init ECMP

---
+++ ns2/classifier/classifier-mpath.cc
@@ -0,0 +1,128 @@
+/* -*-	Mode:C++; c-basic-offset:8; tab-width:8; indent-tabs-mode:t -*- */
+
+/*
+ * Copyright (C) 1997 by the University of Southern California
+ * $Id: classifier-mpath.cc,v 1.10 2005/08/25 18:58:01 johnh Exp $
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *
+ * The copyright of this module includes the following
+ * linking-with-specific-other-licenses addition:
+ *
+ * In addition, as a special exception, the copyright holders of
+ * this module give you permission to combine (via static or
+ * dynamic linking) this module with free software programs or
+ * libraries that are released under the GNU LGPL and with code
+ * included in the standard release of ns-2 under the Apache 2.0
+ * license or under otherwise-compatible licenses with advertising
+ * requirements (or modified versions of such code, with unchanged
+ * license).  You may copy and distribute such a system following the
+ * terms of the GNU GPL for this module and the licenses of the
+ * other code concerned, provided that you include the source code of
+ * that other code when and as the GNU GPL requires distribution of
+ * source code.
+ *
+ * Note that people who make modified versions of this module
+ * are not obligated to grant this special exception for their
+ * modified versions; it is their choice whether to do so.  The GNU
+ * General Public License gives permission to release a modified
+ * version without this exception; this exception also makes it
+ * possible to release a modified version which carries forward this
+ * exception.
+ *
+ */
+
+#ifndef lint
+static const char rcsid[] =
+    "@(#) $Header: /cvsroot/nsnam/ns-2/classifier/classifier-mpath.cc,v 1.10 2005/08/25 18:58:01 johnh Exp $ (USC/ISI)";
+#endif
+
+#include "classifier.h"
+#include "ip.h"
+
+struct hkey
+{
+        nsaddr_t src, dst;
+        int fid, ttl;
+};
+
+static unsigned int HashString(const char *bytes, int length)
+{
+        unsigned int result = 0;
+
+        for (int i = 0;  i < length;  i++)
+                result += (result << 3) + *bytes++;
+
+        return result;
+}
+
+class MultiPathForwarder : public Classifier {
+public:
+	MultiPathForwarder() : ns_(0), perflow_(0), debug_(0) {
+                bind_bool("perflow_", &perflow_);
+                bind_bool("debug_", &debug_);
+        }
+
+	virtual int classify(Packet* p) {
+		int cl;
+
+                if (perflow_) {
+                        hdr_ip* h = hdr_ip::access(p);
+                        struct hkey buf_;
+                        buf_.src = mshift(h->saddr());
+                        buf_.dst = mshift(h->daddr());
+                        buf_.fid = h->flowid();
+                        buf_.ttl = h->ttl();
+
+                        char* bufString = (char*)&buf_;
+                        int length = sizeof(hkey);
+                        unsigned int ms_ = HashString(bufString, length);
+                        ms_ %= (maxslot_ + 1);
+
+                        int fail = ms_;
+                        do {
+                                cl = ms_++;
+                                ms_ %= (maxslot_ + 1);
+                        } while (slot_[cl] == 0 && ms_ != fail);
+
+                        if (debug_) {
+                                printf("src = %u, dst = %u, fid = %d, ttl = %d, ms_= %u, cl = %d\n", \
+                                       buf_.src, buf_.dst, buf_.fid, buf_.ttl, ms_, cl);
+                        }
+
+                } else {
+                        int fail = ns_;
+                        do {
+                                cl = ns_++;
+                                ns_ %= (maxslot_ + 1);
+		        } while (slot_[cl] == 0 && ns_ != fail);
+                }
+
+                return cl;
+	}
+
+private:
+	int ns_;       //next slot to be used
+        int perflow_;   //perflow ECMP (1) or not (0)
+        int debug_;     //whether print necessary debug information
+};
+
+static class MultiPathClass : public TclClass {
+public:
+	MultiPathClass() : TclClass("Classifier/MultiPath") {}
+	TclObject* create(int, const char*const*) {
+		return (new MultiPathForwarder());
+	}
+} class_multipath;
