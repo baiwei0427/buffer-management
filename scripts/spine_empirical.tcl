0f8e89554d2b60c8dae32c639bf3abdfe8992191
wei bai <baiwei0427@sina.com>
2016-08-03 23:13:59 +0800

    Add arguments

--- scripts/spine_empirical.tcl
+++ scripts/spine_empirical.tcl
@@ -1,57 +1,79 @@
 source "tcp-traffic-gen.tcl"
 
 set ns [new Simulator]
-set link_rate 100; #100Gbps
-set mean_link_delay 0.0000002; #0.2us
-set host_delay 0.000012; #12us
-
-set topology_spt 16; #servers per ToR
-set topology_tors 16; #ToR
-set topology_spines 16; #spine (core) switches
-
-set flowlog [open flow.tr w]
-set debug_mode 1
 set sim_start [clock seconds]
-set flow_tot 100000; #total number of flows to generate
+
+if {$argc != 24} {
+    puts "wrong number of arguments $argc"
+    exit 0
+}
+
+#### Topology
+set link_rate [lindex $argv 0]; #link rate (Gbps)
+set mean_link_delay [lindex $argv 1]; #link propagation + processing delay
+set host_delay [lindex $argv 2]; #processing delay at end host
+set topology_spt [lindex $argv 3]; #number of servers per ToR
+set topology_tors [lindex $argv 4]; #number of ToR switches
+set topology_spines [lindex $argv 5]; #number of spine (core) switches
+
+#### Traffic
+set flow_tot [lindex $argv 6]; #total number of flows to generate
+set num_pairs [lindex $argv 7]; #number of senders that a host can receive traffic from
+set connections_per_pair [lindex $argv 8]; #the number of parallel connections for each sender-receiver pair
+set load [lindex $argv 9]; #average utilization of server-ToR links
+set flow_cdf [lindex $argv 10]; #file of flow size CDF
+set mean_flow_size [lindex $argv 11]; #average size of the above distributio
+
+#### Transport settings options
+set enable_ecn [lindex $argv 12];
+set enable_dctcp [lindex $argv 13]
+set init_window [lindex $argv 14]
+set packet_size [lindex $argv 15]; #packet size in bytes
+set rto_min [lindex $argv 16]
+
+#### Switch side options
+set switch_alg [lindex $argv 17]
+set static_port_pkt [lindex $argv 18]; #static per-port buffer size in packets
+set shared_port_bytes [lindex $argv 19]; #dynamic per-port average buffer size in bytes
+set enable_shared_buf [lindex $argv 20]; #enable shared buffer management or not
+set dt_alpha [lindex $argv 21]; #alpha for DT algorithm
+set ecn_thresh [lindex $argv 22]; #ECN marking threshold in packets
+
+### result file
+set flowlog [open [lindex $argv 23] w]
+
+set debug_mode 1
 set flow_gen 0; #the number of flows that have been generated
 set flow_fin 0; #the number of flows that have finished
-set packet_size 8960; #Jumbo packet (9KB)
 set source_alg Agent/TCP/FullTcp/Sack
-set switch_alg DCTCP
-
-set flow_cdf CDF_dctcp.tcl
-set mean_flow_size 1711250
-
-set num_pairs 64
-set connections_per_pair 10
-set load 0.6; # load of edge links
-
-set buffer_size 300; # 300 MTU = 2.7MB
-set rto_min 0.005; # 5ms
-set ecn_thresh 60; # 60 MTU = 540KB, BDP = 625KB
 
 ################ Shared Buffer ##################
-set avg_port_buf 250000; # 250KB
-set tor_shared_buf [expr ($topology_spt + $topology_spines) * $avg_port_buf]
-set spine_shared_buf [expr $topology_tors * $avg_port_buf]
-
-puts "shared buffer of ToR switches: $tor_shared_buf"
-puts "shared buffer of spine switches: $spine_shared_buf"
+set tor_shared_buf [expr ($topology_spt + $topology_spines) * $shared_port_bytes]
+set spine_shared_buf [expr $topology_tors * $shared_port_bytes]
+puts "Shared buffer of ToR switches: $tor_shared_buf"
+puts "Shared buffer of spine switches: $spine_shared_buf"
 
 ################## TCP #########################
-Agent/TCP set ecn_ 1
-Agent/TCP set old_ecn_ 1
-Agent/TCP set dctcp_ true
+if {$enable_ecn == 1} {
+        Agent/TCP set ecn_ 1
+        Agent/TCP set old_ecn_ 1
+        puts "Enable ECN"
+}
+
+if {$enable_dctcp == 1} {
+        Agent/TCP set dctcp_ true
+        puts "Enable DCTCP"
+}
+
 Agent/TCP set dctcp_g_ 0.0625
-Agent/TCP set windowInit_ 16
+Agent/TCP set windowInit_ $init_window
 Agent/TCP set packetSize_ $packet_size
-Agent/TCP set window_ 1000
+Agent/TCP set window_ 1256
 Agent/TCP set slow_start_restart_ true
 Agent/TCP set tcpTick_ 0.000001; # 1us should be enough
 Agent/TCP set minrto_ $rto_min
 Agent/TCP set rtxcur_init_ $rto_min; # initial RTO
 Agent/TCP set maxrto_ 64
-Agent/TCP set numdupacks_ 3; # dup ACK threshold
 Agent/TCP set windowOption_ 0
 
 Agent/TCP/FullTcp set nodelay_ true; # disable Nagle
@@ -60,7 +82,7 @@ Agent/TCP/FullTcp set segsperack_ 1; # ACK frequency
 Agent/TCP/FullTcp set interval_ 0.000006; #delayed ACK interval
 
 ################ Queue #########################
-Queue set limit_ $buffer_size
+Queue set limit_ $static_port_pkt
 Queue/RED set bytes_ false
 Queue/RED set queue_in_bytes_ true
 Queue/RED set mean_pktsize_ [expr $packet_size + 40]
@@ -71,12 +93,18 @@ Queue/RED set mark_p_ 1.0
 Queue/RED set thresh_ $ecn_thresh
 Queue/RED set maxthresh_ $ecn_thresh
 
+if {$enable_shared_buf == 1} {
+        Queue/DCTCP set enable_shared_buf_ true
+        puts "Enable shared buffer"
+}
 Queue/DCTCP set thresh_ $ecn_thresh
 Queue/DCTCP set mean_pktsize_ [expr $packet_size + 40]
-Queue/DCTCP set enable_shared_buf_ true
 Queue/DCTCP set shared_buf_id_ -1
-Queue/DCTCP set alpha_ 1
+Queue/DCTCP set alpha_ $dt_alpha
 Queue/DCTCP set debug_ false
+Queue/DCTCP set pkt_tot_ 0
+Queue/DCTCP set pkt_drop_ 0
+Queue/DCTCP set pkt_drop_ecn_ 0
 
 ################ Multipathing ###########################
 $ns rtproto DV
@@ -90,6 +118,13 @@ Classifier/MultiPath set debug_ false
 
 ######################## Topoplgy #########################
 set topology_servers [expr $topology_spt * $topology_tors]; #number of servers
+set topology_x [expr ($topology_spt + 0.0) / $topology_spines]
+
+puts "$topology_servers servers in total, $topology_spt servers per rack"
+puts "$topology_tors ToR switches"
+puts "$topology_spines spine switches"
+puts "Oversubscription ratio $topology_x"
+flush stdout
 
 for {set i 0} {$i < $topology_servers} {incr i} {
         set s($i) [$ns node]
@@ -103,6 +138,8 @@ for {set i 0} {$i < $topology_spines} {incr i} {
         set spine($i) [$ns node]
 }
 
+set qid 0
+
 ############ Edge links ##############
 for {set i 0} {$i < $topology_servers} {incr i} {
         set j [expr $i/$topology_spt]; # ToR ID
@@ -114,6 +151,8 @@ for {set i 0} {$i < $topology_servers} {incr i} {
         $q set shared_buf_id_ $j
         $q set-shared-buffer $j $tor_shared_buf
         $q register
+        set queues($qid) $q
+        incr qid
 }
 
 ############ Core links ##############
@@ -127,6 +166,8 @@ for {set i 0} {$i < $topology_tors} {incr i} {
                 $q set shared_buf_id_ $i
                 $q set-shared-buffer $i $tor_shared_buf
                 $q register
+                set queues($qid) $q
+                incr qid
 
                 ######## configure shared buffer for spine to ToR links ########
                 set L [$ns link $spine($j) $tor($i)]
@@ -134,6 +175,8 @@ for {set i 0} {$i < $topology_tors} {incr i} {
                 $q set shared_buf_id_ [expr $j + $topology_tors]
                 $q set-shared-buffer [expr $j + $topology_tors] $spine_shared_buf
                 $q register
+                set queues($qid) $q
+                incr qid
         }
 }
 
